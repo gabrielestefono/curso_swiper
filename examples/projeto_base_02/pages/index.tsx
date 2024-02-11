f2524c9436b1dbfe6677d361c8d0430685362126
gabrielestefono <gabrielestefono@hotmail.com>
2024-02-11 01:40:04 -0300

    Salvando Projeto Base 02

---
+++ examples/projeto_base_02/pages/index.tsx
@@ -0,0 +1,10 @@
+import CarouselPrincipal from "@/components/home/CarouselPrincipal";
+import Layout from "@/components/layout/Layout";
+
+export default function Home() {
+  return (
+      <Layout>
+        <CarouselPrincipal/>
+      </Layout>
+  );
+}
